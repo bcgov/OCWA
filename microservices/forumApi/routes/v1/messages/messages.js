49627b9464cc36bd6ea48b65456403539cb4edcf
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-09-19 09:41:58 -0700

    websocket fixes

--- microservices/forumApi/routes/v1/messages/messages.js
+++ microservices/forumApi/routes/v1/messages/messages.js
@@ -1,27 +1,28 @@
 var messages = {};
+var logger = require('npmlog');
 
-function checkTopicPermissions(user, topicId, cb){
+function checkTopicPermissions(user, topicId, sock, cb){
 
     var db = require('../db/db');
     var mongoose = require('mongoose');
     var q = new mongoose.Query();
     q.and([
         { $or: [{topic_id: topicId}, {topic_id: '*'}] },
-        { $or: [{user_id: user.id}, {user_id: '*'}, {group_id: {$in: user.groups}}, {group_id: "*"}]},
+        { $or: [{user_id: user.id}, {user_id: '*'}, {group_ids: {$in: user.Groups}}, {group_ids: "*"}]},
         { allow: true }
     ]);
 
     db.Permission.find(q).sort('priority').exec(function(err, results){
         if (err || !results || results.length === 0){
-            cb(false);
+            cb(false, sock);
             return;
         }
-        cb(true);
+        cb(true, sock);
     });
 }
 
-function checkCommentPermissions(user, topicId, commentId, cb){
-    checkTopicPermissions(user, topicId, function(send){
+function checkCommentPermissions(user, topicId, commentId, sock, cb){
+    checkTopicPermissions(user, topicId, sock, function(send, s){
         if (!send){
             return false;
         }
@@ -31,15 +32,17 @@ function checkCommentPermissions(user, topicId, commentId, cb){
         var q = new mongoose.Query();
         q.and([
             { $or: [{comment_id: commentId}, {comment_id: '*'}] },
-            { $or: [{user_id: user.id}, {user_id: '*'}, {group_id: {$in: user.groups}}, {group_id: "*"}]},
+            { $or: [{user_id: user.id}, {user_id: '*'}, {group_id: {$in: user.Groups}}, {group_id: "*"}]},
             { allow: false }
         ]);
 
         db.Permission.find(q).sort('priority').exec(function(err, results){
             if (err || ( (typeof(results) === "object") && (results.length > 0) )){
-                cb(false);
+                console.log(user);
+                cb(false, sock);
+                return;
             }
-            cb(true);
+            cb(true, sock);
         });
 
     })
@@ -51,9 +54,9 @@ function sendTopicMessage(topic){
     var conns = websockets.getConnections();
     var keys = Object.keys(conns);
     for (var i=0; i<keys.length; i++){
-        checkTopicPermissions(conns[keys[i]].user, topic._id, function(send) {
+        checkTopicPermissions(conns[keys[i]].user, topic._id, function(send, sock) {
             if (send) {
-                conns[keys[i]].send({topic: JSON.stringify(topic)});
+                sock.send({topic: JSON.stringify(topic)});
             }
         });
     }
@@ -69,16 +72,16 @@ function sendCommentMessage(topicId, comment){
     var conns = websockets.getConnections();
     var keys = Object.keys(conns);
     for (var i=0; i<keys.length; i++){
-        checkCommentPermissions(conns[keys[i]].user, topicId, comment._id, function(send) {
+        checkCommentPermissions(conns[keys[i]].user, topicId, comment._id, conns[keys[i]], function(send, sock) {
             if (send) {
-                conns[keys[i]].send(JSON.stringify({comment: comment}));
+                sock.send(JSON.stringify({comment: comment}));
             }
         });
     }
 }
 
 messages.sendCommentMessage = function(topic, comment){
-    setTimeout(sendCommentMessage, 0, topic, comment);
+    setTimeout(sendCommentMessage, 1, topic, comment);
 };
 
 module.exports = messages;
