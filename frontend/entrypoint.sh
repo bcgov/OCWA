30e5673f73ee082c1fb705d70561eed4681cdc76
Joshua <joshua@general-metrics.com>
2018-09-24 15:49:59 -0700

    Add socket host variable separate from the api host, which uses docker.

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,6 +4,7 @@ mkdir -p ./config
 printf "{\n" > ./config/default.json
 printf "\"port\": \"${PORT}\",\n" >> ./config/default.json
 printf "\"host\": \"${HOST}\",\n" >> ./config/default.json
+printf "\"socketHost\": \"${SOCKET_HOST}\",\n" >> ./config/default.json
 printf "\"jwtSecret\": \"${JWT_SECRET}\",\n" >> ./config/default.json
 printf "\"jwt\": \"${JWT}\",\n" >> ./config/default.json
 printf "\"user\": {\n" >> ./config/default.json
