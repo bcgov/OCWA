41cd9468be8655e5d61c243d3abcd672684ae08d
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-09-25 15:06:02 -0700

    permissions tweaks

--- microservices/forumApi/routes/v1/routes/comments.js
+++ microservices/forumApi/routes/v1/routes/comments.js
@@ -77,7 +77,7 @@ router.post("/:topicId", function(req, res, next){
             log.debug("comment saved triggering websocket");
             messages.sendCommentMessage(topicId, result);
 
-            res.json({message: "Successfully written"});
+            res.json({message: "Successfully written", _id: result._id});
         });
     });
 
