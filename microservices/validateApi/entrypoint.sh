01107872c638f21902b5b07daa0b36b179505614
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-10-01 16:17:19 -0700

    charts charts charts

--- microservices/validateApi/entrypoint.sh
+++ microservices/validateApi/entrypoint.sh
@@ -2,20 +2,30 @@
 
 mkdir -p ./config
 printf "{\n" > ./config/default.json
-printf "\"apiPort\": 3000,\n" >> ./config/default.json
-printf "\"wsPort\": 3001,\n" >> ./config/default.json
+printf "\"apiPort\": 3003,\n" >> ./config/default.json
 printf "\"logLevel\": \"${LOG_LEVEL}\",\n" >> ./config/default.json
-printf "\"morganLogType\": \"dev\",\n" >> ./config/default.json
+
 printf "\"database\": {\n" >> ./config/default.json
 printf "\"host\": \"${DB_HOST}\",\n" >> ./config/default.json
+printf "\"port\": \"${DB_PORT}\",\n" >> ./config/default.json
 printf "\"username\": \"${DB_USERNAME}\",\n" >> ./config/default.json
 printf "\"password\": \"${DB_PASSWORD}\",\n" >> ./config/default.json
 printf "\"dbName\": \"${DB_NAME}\"\n" >> ./config/default.json
 printf "},\n" >> ./config/default.json
-printf "\"jwtSecret\": \"${JWT_SECRET}\",\n" >> ./config/default.json
+
+printf "\"storage\": {\n" >> ./config/default.json
+printf "\"endpoint\": \"${STORAGE_HOST}\",\n" >> ./config/default.json
+printf "\"bucket\": \"${STORAGE_BUCKET}\",\n" >> ./config/default.json
+printf "\"access_key_id\": \"${STORAGE_ACCESS_KEY}\",\n" >> ./config/default.json
+printf "\"access_secret_id\": \"${STORAGE_ACCESS_SECRET}\",\n" >> ./config/default.json
+printf "},\n" >> ./config/default.json
+
+printf "\"policyApi\": \"${POLICY_URL}\",\n" >> ./config/default.json
+
+printf "\"apiSecret\": \"${API_SECRET}\",\n" >> ./config/default.json
 printf "\"user\": {\n" >> ./config/default.json
 printf "\"idField\": \"${USER_ID_FIELD}\"\n" >> ./config/default.json
 printf "}\n" >> ./config/default.json
 printf "}" >> ./config/default.json
 
-npm start
+python wsgi.py
