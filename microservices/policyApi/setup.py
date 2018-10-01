44a3f17b10fcf492df32005ef1271270120d1c71
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-10-01 09:08:47 -0700

    travis tweaks

--- microservices/policyApi/setup.py
+++ microservices/policyApi/setup.py
@@ -1,11 +1,28 @@
 import os
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 
 def read(filename):
     file_path = os.path.join(os.path.dirname(__file__), filename)
     return open(file_path).read()
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex
+
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
 
 
 setup(
@@ -37,5 +54,6 @@ setup(
         'pytest-cov',
         'pycodestyle',
         'pylint'
-    ]
+    ],
+    cmdclass={"pytest": PyTest},
 )
