858d739ddf7e0d1e94a836fb5208922a4702d64e
Joshua <joshua@general-metrics.com>
2018-10-19 13:32:19 -0700

    Keep working the form.

--- frontend/src/modules/requests/components/new-request/index.jsx
+++ frontend/src/modules/requests/components/new-request/index.jsx
@@ -1,35 +1,26 @@
 import * as React from 'react';
 import Button from '@atlaskit/button';
-import Modal, { ModalTransition } from '@atlaskit/modal-dialog';
+import { Checkbox } from '@atlaskit/checkbox';
+import isEmpty from 'lodash/isEmpty';
 import FieldText from '@atlaskit/field-text';
 import FieldTextArea from '@atlaskit/field-text-area';
-import Form, {
-  Field,
-  FormHeader,
-  FormSection,
-  FormFooter,
-  Validator,
-} from '@atlaskit/form';
+import Form, { Field, FormSection, Validator } from '@atlaskit/form';
+import Modal, { ModalFooter, ModalTransition } from '@atlaskit/modal-dialog';
 
 class NewRequest extends React.Component {
   state = {
     open: false,
-    name: '',
-    tags: '',
-    purpose: '',
-    variableDescriptions: '',
-    selectionCriteria: '',
-    steps: '',
-    freq: '',
-    confidentiality: '',
+    isTermsAccepted: false,
   };
 
-  onChange = () => {
-    console.log(this.formRef.validate());
-  };
+  onSubmit = event => {
+    event.preventDefault();
+    const form = this.formRef.validate();
+    console.log(form);
 
-  onSubmit = (...args) => {
-    console.log(args);
+    if (!form.isInvalid) {
+      console.log('process!');
+    }
   };
 
   onToggleDialog = () => {
@@ -38,22 +29,25 @@ class NewRequest extends React.Component {
     }));
   };
 
+  onTermsChange = () => {
+    this.setState(state => ({
+      isTermsAccepted: !state.isTermsAccepted,
+    }));
+  };
+
+  renderFooter = () => (
+    <ModalFooter>
+      <Button appearance="subtle" onClick={this.onToggleDialog}>
+        Cancel
+      </Button>
+      <Button appearance="primary" onClick={this.onSubmit}>
+        Submit
+      </Button>
+    </ModalFooter>
+  );
+
   render() {
-    const {
-      open,
-      name,
-      tags,
-      purpose,
-      variableDescriptions,
-      selectionCriteria,
-      steps,
-      freq,
-      confidentiality,
-    } = this.state;
-    const actions = [
-      { text: 'Cancel', onClick: this.onToggleDialog },
-      { text: 'Next Step', onClick: this.onToggleDialog },
-    ];
+    const { open, isTermsAccepted } = this.state;
 
     return (
       <React.Fragment>
@@ -63,7 +57,7 @@ class NewRequest extends React.Component {
         <ModalTransition>
           {open && (
             <Modal
-              actions={actions}
+              footer={this.renderFooter}
               heading="Initiate a New Request"
               width="x-large"
             >
@@ -73,46 +67,83 @@ class NewRequest extends React.Component {
                   this.formRef = form;
                 }}
               >
-                <Field label="Request Name" isRequired>
+                <Field isRequired label="Request Name">
                   <FieldText
                     shouldFitContainer
-                    onChange={this.onChange}
-                    value={name}
+                    name="name"
+                    id="name"
+                    value=""
                   />
                 </Field>
-                <Field label="Purpose" helperText="Purpose of the request">
-                  <FieldTextArea shouldFitContainer value={purpose} />
-                </Field>
-                <Field
-                  label="Variable Descriptions"
-                  helperText="Description of variables in the request"
+                <FormSection
+                  name="additional"
+                  title="Additional Information"
+                  description="These fields aren't required but are recommended"
                 >
-                  <FieldTextArea
-                    shouldFitContainer
-                    value={variableDescriptions}
-                  />
-                </Field>
-                <Field
-                  label="Selection Criteria"
-                  helperText="Selection criteria and sample size description for the request"
-                >
-                  <FieldTextArea shouldFitContainer value={selectionCriteria} />
-                </Field>
-                <Field label="Steps" helperText="Annotation of steps taken">
-                  <FieldTextArea shouldFitContainer value={steps} />
-                </Field>
-                <Field
-                  label="Frequency"
-                  helperText="Weighted results and unweighted frequencies"
-                >
-                  <FieldTextArea shouldFitContainer value={freq} />
-                </Field>
-                <Field
-                  label="Confidentiality"
-                  helperText="Confidentiality disclosure to describe how it's upheld when criteria isn't met"
-                >
-                  <FieldTextArea shouldFitContainer value={confidentiality} />
-                </Field>
+                  <Field label="Purpose" helperText="Purpose of the request">
+                    <FieldTextArea shouldFitContainer name="purpose" value="" />
+                  </Field>
+                  <Field
+                    label="Variable Descriptions"
+                    helperText="Description of variables in the request"
+                  >
+                    <FieldTextArea
+                      shouldFitContainer
+                      name="variableDescriptions"
+                      value=""
+                    />
+                  </Field>
+                  <Field
+                    label="Selection Criteria"
+                    helperText="Selection criteria and sample size description for the request"
+                  >
+                    <FieldTextArea
+                      shouldFitContainer
+                      name="selectionCriteria"
+                      value=""
+                    />
+                  </Field>
+                  <Field label="Steps" helperText="Annotation of steps taken">
+                    <FieldTextArea shouldFitContainer name="steps" value="" />
+                  </Field>
+                  <Field
+                    label="Frequency"
+                    helperText="Weighted results and unweighted frequencies"
+                  >
+                    <FieldTextArea shouldFitContainer name="freq" value="" />
+                  </Field>
+                  <Field
+                    label="Confidentiality"
+                    helperText="Confidentiality disclosure to describe how it's upheld when criteria isn't met"
+                  >
+                    <FieldTextArea
+                      shouldFitContainer
+                      name="confidentiality"
+                      value=""
+                    />
+                  </Field>
+                </FormSection>
+                <FormSection>
+                  <Field
+                    validateOnChange
+                    isRequired
+                    validators={[
+                      <Validator
+                        func={() => isTermsAccepted}
+                        invalid="You must check this box to submit your request."
+                      />,
+                    ]}
+                  >
+                    <Checkbox
+                      isChecked={isTermsAccepted}
+                      onChange={this.onTermsChange}
+                      label="By checking this box I affirm that requested output is safe for release and protects the confidentiality of data, to the best of the authorized user’s knowledge"
+                      value="isAffirmed"
+                      id="affirmation-checkbox"
+                      name="affirmation-checkbox"
+                    />
+                  </Field>
+                </FormSection>
               </Form>
             </Modal>
           )}
