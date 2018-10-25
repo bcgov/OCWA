14b322fe538598d17436ec2a0d0dbd1e55c45103
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-10-25 11:31:21 -0700

    Added OCP documentation, tweaked how request/forum api interact with jwt tokens, added a required group for creating topics, modified permissions to have default allow for either individual or group via config. Updated helm charts, docker to capture these changes

--- microservices/forumApi/entrypoint.sh
+++ microservices/forumApi/entrypoint.sh
@@ -13,8 +13,15 @@ printf "\"password\": \"${DB_PASSWORD}\",\n" >> ./config/default.json
 printf "\"dbName\": \"${DB_NAME}\"\n" >> ./config/default.json
 printf "},\n" >> ./config/default.json
 printf "\"jwtSecret\": \"${JWT_SECRET}\",\n" >> ./config/default.json
+printf "\"defaultAccessIsGroup\": ${DEFAULT_ACCESS_IS_GROUP},\n" >> ./config/default.json
+printf "\"requiredRoleToCreateTopic\": \"${REQUIRED_CREATE_ROLE}\",\n" >> ./config/default.json
+
 printf "\"user\": {\n" >> ./config/default.json
 printf "\"idField\": \"${USER_ID_FIELD}\"\n" >> ./config/default.json
+printf "\"emailField\": \"${EMAIL_FIELD}\"\n" >> ./config/default.json
+printf "\"givenNameField\": \"${GIVENNAME_FIELD}\"\n" >> ./config/default.json
+printf "\"surNameField\": \"${SURNAME_FIELD}\"\n" >> ./config/default.json
+printf "\"groupField\": \"${GROUP_FIELD}\"\n" >> ./config/default.json
 printf "}\n" >> ./config/default.json
 printf "}" >> ./config/default.json
 
