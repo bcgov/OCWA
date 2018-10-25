abf86b159f6a9eea52b99b3dd46e492e3a896e55
Joshua <joshua@general-metrics.com>
2018-10-25 10:47:48 -0700

    Doctor the signing of JWTs to work with the microservices auth.

--- frontend/server/routes/auth.js
+++ frontend/server/routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const get = require('lodash/get');
 const isEmpty = require('lodash/isEmpty');
 const jwt = require('jsonwebtoken');
+const mapKeys = require('lodash/mapKeys');
 const passport = require('passport');
 
 const router = express.Router();
@@ -15,20 +16,30 @@ router.get(
   }
 );
 
-// Return the session for the
+// Return the session token
 router.get('/session', (req, res) => {
   const jwtSecret = config.get('jwtSecret');
   let token = null;
 
-  // If there is now jwtSecret defined go with OCID
+  // If there is now jwtSecret defined go with OCID only
   if (isEmpty(jwtSecret)) {
     if (req.isAuthenticated()) {
       token = req.user.accessToken;
     }
   } else {
+    // Passport/KeyCloak doesn't sign the token correctly so some fixing is needed.
     const jwtClaims = get(req, 'user.claims');
     if (jwtClaims) {
-      token = jwt.sign(jwtClaims, jwtSecret);
+      // Create a normalized JWT that works with the microservices auth.
+      // It needs an `Email` and `Groups` property.
+      const data = {
+        ...jwtClaims,
+        Email: jwtClaims.email,
+        Groups: [],
+      };
+      delete data.email;
+
+      token = jwt.sign(data, jwtSecret);
     }
   }
 
