c47f7c91093217d700a89c682c1afef59766720f
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-10-30 15:12:38 -0700

    request api overhaul

--- microservices/requestApi/test/v1/requests.js
+++ microservices/requestApi/test/v1/requests.js
@@ -66,7 +66,7 @@ describe("Requests", function() {
                     confidentiality: "none"
                 })
                 .end(function (err, res) {
-                    res.should.have.status(200);
+                    res.should.have.status(500);
                     res.body.should.be.a('object');
                     res.body.should.have.property('error');
                     res.body.error.should.be.a('object');
