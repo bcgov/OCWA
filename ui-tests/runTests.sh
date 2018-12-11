04a4e9e609e0dc1a1090eed9d5b12d2efce08e5e
PaulR <PaulR@TPL0044.ad.avocette.com>
2018-12-11 10:34:16 -0800

    run tests

--- ui-tests/runTests.sh
+++ ui-tests/runTests.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
 katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/ui-tests/OCWA.prj" -retry=0 -testSuitePath="Test Suites/Run OCWA test cases" -executionProfile="default" -browserType="Chrome (headless)"
