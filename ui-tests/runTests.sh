1d7b8b6bafcce7aedcf75ecb808379bc211d9f9b
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-12-11 11:02:04 -0800

    fixed line endings and project definition for automated tests

--- ui-tests/runTests.sh
+++ ui-tests/runTests.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 echo "Starting tests"
 
 katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/ui-tests/OCWA.prj" -retry=0 -testSuitePath="Test Suites/Run OCWA test cases" -executionProfile="default" -browserType="Chrome (headless)"
