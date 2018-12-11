ce7838c26eb0d72e46f40fd4dac86457d35f7741
Brandon Sharratt <brandon@highwaythreesolutions.com>
2018-12-11 11:18:32 -0800

    removed shebang as travis doesn't seem to have bin/bash

--- ui-tests/runTests.sh
+++ ui-tests/runTests.sh
@@ -1,4 +1,3 @@
-#!bin/bash
 echo "Starting tests"
 
 katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/ui-tests/OCWA.prj" -retry=0 -testSuitePath="Test Suites/Run OCWA test cases" -executionProfile="default" -browserType="Chrome (headless)"
