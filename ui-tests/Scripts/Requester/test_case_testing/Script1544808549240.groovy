a6add4d4b43ce4297b7a3d8d02c312210c17aeff
PaulR <PaulR@TPL0044.ad.avocette.com>
2018-12-17 11:01:23 -0800

    try an expanded non-cucumber test

--- ui-tests/Scripts/Requester/test_case_testing/Script1544808549240.groovy
+++ ui-tests/Scripts/Requester/test_case_testing/Script1544808549240.groovy
@@ -13,9 +13,11 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('')
+WebUI.openBrowser('http://ocwa-cddi-dlt-dev.pathfinder.gov.bc.ca/')
 
-WebUI.delay(5)
+WebUI.navigateToUrl(GlobalVariable.OCWA_URL)
+
+WebUI.click(findTestObject('Object Repository/Page_OCWA Development Version/span_Login'))
 
 WebUI.closeBrowser()
 
