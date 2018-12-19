a4d0466691cc0abb7917b12881874f6a4bc4857c
PaulR <PaulR@TPL0044.ad.avocette.com>
2018-12-19 15:23:49 -0800

    fixed typo in shell file

--- ui_tests/runTests.sh
+++ ui_tests/runTests.sh
@@ -1,3 +1,3 @@
 echo "Starting tests"
 
-katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/ui_tests/OCWA.prj" -retry=0 -testSuitePath="Test Suites/CucumberSuites" -executionProfile="Travis" -browserType="Chrome (headless)"
+katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/ui_tests/OCWA.prj" -retry=0 -testSuitePath="Test Suites/CucumberSuite" -executionProfile="Travis" -browserType="Chrome (headless)"
