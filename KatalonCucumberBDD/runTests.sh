9b9f08c60f820071b9ae8accba6271e06885053f
PaulR <PaulR@TPL0044.ad.avocette.com>
2018-12-20 14:43:00 -0800

    fixed executionProfile

--- KatalonCucumberBDD/runTests.sh
+++ KatalonCucumberBDD/runTests.sh
@@ -1,3 +1,3 @@
 echo "Starting tests"
 
-katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/KatalonCucumberBDD/KatalonCucumber.prj" -retry=0 -testSuitePath="Test Suites/TestSuite1" -executionProfile="Travis" -browserType="Chrome (headless)"
+katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/KatalonCucumberBDD/KatalonCucumber.prj" -retry=0 -testSuitePath="Test Suites/TestSuite1" -executionProfile="default" -browserType="Chrome (headless)"
