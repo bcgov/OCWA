89f5d75bd06d0f8c8fe991ee15226b76a871af61
PaulR <PaulR@TPL0044.ad.avocette.com>
2018-12-19 16:13:48 -0800

    add additional cucumber test suite

---
+++ ui_tests/Include/scripts/groovy/com/ea/steps/CucumberEARunner.groovy
@@ -0,0 +1,14 @@
+package com.ea.steps
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Include/features", glue="", plugin=["pretty", "html:ReportFolder", "json:ReportFolder/cucumber.json"])
+public class CucumberEARunner {
+}
