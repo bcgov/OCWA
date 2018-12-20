ffd45d9f6092fccec659adb70da07661d6074b73
PaulR <PaulR@TPL0044.ad.avocette.com>
2018-12-19 16:13:48 -0800

    add additional cucumber test suite

--- ui_tests/runTests.sh
+++ ui_tests/runTests.sh
@@ -1,3 +1,3 @@
 echo "Starting tests"
 
-katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/ui_tests/OCWA.prj" -retry=0 -testSuitePath="Test Suites/CucumberSuite" -executionProfile="Travis" -browserType="Chrome (headless)"
+katalon -noSplash  -runMode=console -projectPath="/home/travis/build/bcgov/OCWA/ui_tests/OCWA.prj" -retry=0 -testSuitePath="Test Suites/TestSuite1" -executionProfile="Travis" -browserType="Chrome (headless)"
