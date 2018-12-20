4dbfdb3cf9dadbb4372aebfecd74892bf92df39c
PaulR <PaulR@TPL0044.ad.avocette.com>
2018-12-20 13:57:45 -0800

    try out BDD example project in Travis

---
+++ KatalonCucumberBDD/runTests.bat
@@ -0,0 +1,3 @@
+echo "Starting tests"
+
+C:\Users\PaulR\Documents\Katalon_Studio_Windows_64-5.9.0\Katalon_Studio_Windows_64-5.9.0\katalon -noSplash  -runMode=console -projectPath="C:\Users\PaulR\Documents\OCWA\OCWA\KatalonCucumberBDD\KatalonCucumber.prj" -retry=0 -testSuitePath="Test Suites/TestSuite1" -executionProfile="default" -browserType="Chrome (headless)"
