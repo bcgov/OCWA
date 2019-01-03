b068bc3663030c544bb046f42e94b27ad59578d2
ikethecoder <ikethecoder@canzea.com>
2019-01-03 14:41:29 -0800

    Use terraform environment for integration tests

--- terraform/ocwa_policy_api.tf
+++ terraform/ocwa_policy_api.tf
@@ -1,6 +1,6 @@
 
 data "docker_registry_image" "ocwa_policy_api" {
-  name = "bcgovimages/ocwa_policy_api:edge"
+  name = "bcgovimages/ocwa_policy_api${var.images["policy_api"]}"
 }
 
 resource "docker_image" "ocwa_policy_api" {
@@ -11,17 +11,21 @@ resource "docker_image" "ocwa_policy_api" {
 resource "docker_container" "ocwa_policy_api" {
   image = "${docker_image.ocwa_policy_api.latest}"
   name = "ocwa_policy_api"
+  restart = "on-failure"
   networks_advanced = { name = "${docker_network.private_network.name}" }
   env = [
+      "LOG_LEVEL=info",
       "JWT_SECRET=${random_string.jwtSecret.result}",
       "API_SECRET=${random_string.apiSecret.result}",
       "API_PORT=3004",
       "DB_HOST=ocwa_mongodb",
       "DB_NAME=oc_db",
+      "DB_PORT=27017",
       "DB_USERNAME=${var.mongodb["username"]}",
       "DB_PASSWORD=${random_string.mongoSuperPassword.result}",
       "JWT_AUD=aud",
       "JWT_ACCESS_GROUP=admin",
-      "JWT_GROUPS=groups"
+      "JWT_GROUPS=groups",
+      "USER_ID_FIELD=email"
   ]
 }
--- terraform/ocwa_request_api.tf
+++ terraform/ocwa_request_api.tf
@@ -1,6 +1,6 @@
 
 data "docker_registry_image" "ocwa_request_api" {
-  name = "bcgovimages/ocwa_request_api:edge"
+  name = "bcgovimages/ocwa_request_api${var.images["request_api"]}"
 }
 
 resource "docker_image" "ocwa_request_api" {
@@ -11,11 +11,13 @@ resource "docker_image" "ocwa_request_api" {
 resource "docker_container" "ocwa_request_api" {
   image = "${docker_image.ocwa_request_api.latest}"
   name = "ocwa_request_api"
+  restart = "on-failure"
   networks_advanced = { name = "${docker_network.private_network.name}" }
   env = [
       "JWT_SECRET=${random_string.jwtSecret.result}",
       "API_PORT=3002",
       "DB_HOST=ocwa_mongodb",
+      "DB_PORT=27017",
       "DB_NAME=oc_db",
       "DB_USERNAME=${var.mongodb["username"]}",
       "DB_PASSWORD=${random_string.mongoSuperPassword.result}",
@@ -27,17 +29,19 @@ resource "docker_container" "ocwa_request_api" {
       "CREATE_ROLE=/exporter",
       "OC_GROUP=oc",
       "ALLOW_DENY=true",
+      "AUTO_APPROVE=false",
       "VALIDATION_API=http://ocwa_validate_api:3003",
       "VALIDATION_API_KEY=${random_string.apiSecret.result}",
       "FORUM_API=http://ocwa_forum_api:3000",
       "FORUM_API_KEY=${random_string.apiSecret.result}",
-      "STORAGE_URI=ocwa_minio",
+      "STORAGE_URI=ocwaminio",
       "STORAGE_PORT=9000",
       "STORAGE_USESSL=false",
       "STORAGE_BUCKET=bucket",
       "STORAGE_WARN_SIZE=10m",
       "STORAGE_MAX_SIZE=10m",
       "STORAGE_KEY=${random_id.accessKey.hex}",
-      "STORAGE_SECRET=${random_string.secretKey.result}"   
+      "STORAGE_SECRET=${random_string.secretKey.result}"
+      
   ]
 }
