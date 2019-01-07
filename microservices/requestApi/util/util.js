38ee2bc81e3226a88b7a79615b3bab41dad71ce8
Brandon Sharratt <brandon@highwaythreesolutions.com>
2019-01-07 14:33:49 -0800

    chaged scoping of index in util/util

--- microservices/requestApi/util/util.js
+++ microservices/requestApi/util/util.js
@@ -53,53 +53,54 @@ util.getFileStatus = function(fileIds, callback){
     var httpReq = require('request');
 
     for (var i = 0; i < fileIds.length; i++) {
-        log.debug("Attempting to get status for " + fileIds[index]);
-        var index = i;
-        httpReq.get({
-            url: config.get('validationApi') + '/v1/validate/' + fileIds[index],
-            headers: {
-                'X-API-KEY': config.get('validationApiSecret')
-            }
-        }, function (apiErr, apiRes, body) {
-            status[fileIds[index]] = [];
-            if (apiErr || !apiRes) {
-                status[fileIds[index]].push({error: apiErr.message});
-                fullPass = false;
-            } else {
-                // 0 is pass
-                try {
-                    var json = JSON.parse(body);
-                    body = json;
-                    log.debug("Got status for "+fileIds[index]+": ", body);
-
-                    for (var j = 0; j < body.length; j++) {
-
-                        status[fileIds[index]].push({
-                            pass: (body[j].state === 0),
-                            state: body[j].state,
-                            message: body[j].message,
-                            name: body[j].rule_id,
-                            mandatory: body[j].mandatory
-                        });
-
-                        if (body.state !== 0) {
-                            fullPass = false;
-                        }
-                    }
-                }catch(ex){
-                    log.error(ex);
-                    status[fileIds[index]].push({error: "parsing error for validation response."});
-                    fullPass = false;
+        (function(index){
+            log.debug("Attempting to get status for " + fileIds[index]);
+            httpReq.get({
+                url: config.get('validationApi') + '/v1/validate/' + fileIds[index],
+                headers: {
+                    'X-API-KEY': config.get('validationApiSecret')
                 }
-            }
-            numResults++;
-            if (numResults === fileIds.length) {
-                log.debug("Returning all file statuses of:", status);
-                callback(status, fullPass);
-                return;
-            }
+            }, function (apiErr, apiRes, body) {
+                status[fileIds[index]] = [];
+                if (apiErr || !apiRes) {
+                    status[fileIds[index]].push({error: apiErr.message});
+                    fullPass = false;
+                } else {
+                    // 0 is pass
+                    try {
+                        var json = JSON.parse(body);
+                        body = json;
+                        log.debug("Got status for "+fileIds[index]+": ", body);
 
-        });
+                        for (var j = 0; j < body.length; j++) {
+
+                            status[fileIds[index]].push({
+                                pass: (body[j].state === 0),
+                                state: body[j].state,
+                                message: body[j].message,
+                                name: body[j].rule_id,
+                                mandatory: body[j].mandatory
+                            });
+
+                            if (body.state !== 0) {
+                                fullPass = false;
+                            }
+                        }
+                    }catch(ex){
+                        log.error(ex);
+                        status[fileIds[index]].push({error: "parsing error for validation response."});
+                        fullPass = false;
+                    }
+                }
+                numResults++;
+                if (numResults === fileIds.length) {
+                    log.debug("Returning all file statuses of:", status);
+                    callback(status, fullPass);
+                    return;
+                }
+
+            });
+        })(i);
     }
 };
 
