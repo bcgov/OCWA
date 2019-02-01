b0a4c5e5118bc31ba39ad07a155c405eda5f0bfe
PaulR <PaulR@TPL0044.ad.avocette.com>
2019-02-01 15:00:18 -0800

    enabled automated tests for submitting and editing

--- ui_tests/Test Suites/CucumberSuite.ts
+++ ui_tests/Test Suites/CucumberSuite.ts
@@ -4,7 +4,6 @@
    <name>CucumberSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2019-01-03T12:47:19</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
