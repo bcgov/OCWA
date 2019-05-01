dada206a4f3ba81bdd665ad6e40e0828d1ed9e7c
Joshua <joshua@general-metrics.com>
2019-05-01 16:26:51 -0700

    Add some fallbacks for the `Signed in as ...` and remove if there's nothing to show.

--- frontend/src/components/app-bar/menu.jsx
+++ frontend/src/components/app-bar/menu.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import at from 'lodash/at';
 import Avatar from '@atlaskit/avatar';
 import Dropdown, { DropdownItem } from '@atlaskit/dropdown-menu';
+import isEmpty from 'lodash/isEmpty';
 import aboutButton from '@src/modules/app/containers/about-button';
 
 const AboutDropdownItem = props => (
@@ -10,12 +12,23 @@ const AboutDropdownItem = props => (
 const AboutButton = aboutButton(AboutDropdownItem);
 
 function AppBarMenu({ children, user }) {
+  const possibleDisplayNameValues = at(user, [
+    'displayName',
+    'username',
+    'email',
+  ]);
+  const displayName = possibleDisplayNameValues.find(d => !isEmpty(d));
+
   return (
     <Dropdown
       position="bottom right"
-      trigger={<Avatar borderColor="#0052CC" name={user.displayName} />}
+      trigger={<Avatar borderColor="#0052CC" name={displayName || ''} />}
     >
-      <DropdownItem>{`Signed in as ${user.displayName}`}</DropdownItem>
+      {displayName && (
+        <DropdownItem>
+          Signed in as <strong>{displayName}</strong>
+        </DropdownItem>
+      )}
       {children}
       <AboutButton />
       <DropdownItem href="/auth/logout">Logout</DropdownItem>
