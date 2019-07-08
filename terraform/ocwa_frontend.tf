9da94d907c50653279abc846fb612cf986c66415
Ike The Coder <ikethecoder@canzea.com>
2019-07-07 22:04:49 -0700

    Update terraform proxy settings

--- terraform/ocwa_frontend.tf
+++ terraform/ocwa_frontend.tf
@@ -13,10 +13,6 @@ resource "docker_container" "ocwa_download_frontend" {
   name = "ocwa_download_frontend"
   restart = "on-failure"
   networks_advanced = { name = "${docker_network.private_network.name}" }
-  ports = { 
-    internal = 8000
-    external = 8001
-  }
 
   host = [
     {
@@ -37,8 +33,8 @@ resource "docker_container" "ocwa_download_frontend" {
       "AUTH_CALLBACK_URL=${var.ocwaDLHost}/auth",
       "CLIENT_SECRET=${random_uuid.outputcheckerClientSecret.result}",
       "FILES_API_HOST=${var.ocwaDLHost}",
-      "REQUEST_API_HOST=ocwa_request_api:3002",
-      "FORUM_API_HOST=ocwa_forum_api:3000",
+      "REQUEST_API_HOST=http://ocwa_request_api:3002",
+      "FORUM_API_HOST=http://ocwa_forum_api:3000",
       "FORUM_SOCKET_HOST=${var.ocwaWebSocketHost}",
       "HOST=0.0.0.0",
       "PORT=8000",
@@ -62,10 +58,6 @@ resource "docker_container" "ocwa_frontend" {
   name = "ocwa_frontend"
   restart = "on-failure"
   networks_advanced = { name = "${docker_network.private_network.name}" }
-  ports = { 
-    internal = 8000
-    external = 8000
-  }
 
   host = [
     {
@@ -86,8 +78,8 @@ resource "docker_container" "ocwa_frontend" {
       "AUTH_CALLBACK_URL=${var.ocwaHost}/auth",
       "CLIENT_SECRET=${random_uuid.outputcheckerClientSecret.result}",
       "FILES_API_HOST=${var.ocwaHost}",
-      "REQUEST_API_HOST=ocwa_request_api:3002",
-      "FORUM_API_HOST=ocwa_forum_api:3000",
+      "REQUEST_API_HOST=http://ocwa_request_api:3002",
+      "FORUM_API_HOST=http://ocwa_forum_api:3000",
       "FORUM_SOCKET_HOST=${var.ocwaWebSocketHost}",
       "HOST=0.0.0.0",
       "PORT=8000",
