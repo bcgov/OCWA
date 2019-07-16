b124c245920ffdf5da40abb39806d5e2c6d8c18b
Joshua <joshua@general-metrics.com>
2019-07-16 10:10:42 -0700

    Fix unit tests.

--- frontend/tests/globals.js
+++ frontend/tests/globals.js
@@ -2,6 +2,7 @@ global.confirm = jest.fn(() => true);
 global.FILES_API_HOST = 'http://localhost:3000';
 global.SOCKET_HOST = 'ws://localhost:3000';
 global.COMMIT = '98sf9842rse8238ew9r8we9f89';
+global.HELP_URL = 'http://example.com/help';
 global.ID_FIELD = 'preferred_username';
 global.EXPORTER_GROUP = '/exporter';
 global.OC_GROUP = '/oc';
