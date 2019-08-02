0aafd1c1ca41dc5094d86095d6f5214b36aa4452
Paul Ripley <Paul.Ripley@avocette.com>
2019-08-02 10:10:58 -0700

    add happypathsuite and fullfeature testsuites

--- ui_tests/Test Suites/FullFeatureSuite.ts
+++ ui_tests/Test Suites/FullFeatureSuite.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>CucumberSuite</name>
+   <name>FullFeatureSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
