c9368b91c629043d9fb3b79824ca6a3ef6e7de9a
Joshua <joshua@general-metrics.com>
2019-11-25 10:13:55 -0800

    Switch test form over to Form.io component.

--- frontend/src/modules/requests/containers/request-form.js
+++ frontend/src/modules/requests/containers/request-form.js
@@ -5,26 +5,37 @@ import { helpURL } from '@src/services/config';
 import { codeExportEnabled } from '@src/services/config';
 
 import NewRequest from '../components/request-form';
-import { createRequest, fetchRequest } from '../actions';
-import { requestSchema } from '../schemas';
+import { createRequest, fetchForm, fetchRequest } from '../actions';
+import { formSchema, requestSchema } from '../schemas';
 
 const mapStateToProps = state => ({
   helpURL,
   codeExportEnabled,
   newRequestId: state.requests.newRequestId,
   fetchStatus: get(state, 'data.fetchStatus.postRequests.requests', 'idle'),
+  form: get(state, 'data.entities.forms.5dd4773368542243f49c23f9', {}),
 });
 
-export default connect(mapStateToProps, {
-  onFetch: id =>
-    fetchRequest({
-      url: `/api/v1/requests/${id}`,
-      schema: requestSchema,
-      id,
-    }),
-  onCreate: (payload, meta) =>
-    createRequest(payload, meta, {
-      url: '/api/v1/requests',
-      schema: { result: requestSchema },
-    }),
-})(withRequest(NewRequest));
+// ID: 5dd4773368542243f49c23f9
+// Path: data-export
+export default connect(
+  mapStateToProps,
+  {
+    initialRequest: () =>
+      fetchForm({
+        url: '/api/v1/requests/forms/data-export',
+        schema: formSchema,
+      }),
+    onFetch: id =>
+      fetchRequest({
+        url: `/api/v1/requests/${id}`,
+        schema: requestSchema,
+        id,
+      }),
+    onCreate: (payload, meta) =>
+      createRequest(payload, meta, {
+        url: '/api/v1/requests',
+        schema: { result: requestSchema },
+      }),
+  }
+)(withRequest(NewRequest));
