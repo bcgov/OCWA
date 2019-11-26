d456aea77a61269bb26bc63c01a0d72f12a5fb3b
Ike The Coder <ikethecoder@canzea.com>
2019-11-25 20:30:56 -0800

    Remove commented code

--- microservices/policyApi/db/db.py
+++ microservices/policyApi/db/db.py
@@ -4,31 +4,7 @@ from config import Config
 from db.models.rules import Rules
 from db.models.policies import Policies
 
-import logging
 from pymongo import monitoring
-
-log = logging.getLogger(__name__)
-
-# class CommandLogger(monitoring.CommandListener):
-
-#     def started(self, event):
-#         log.debug("Command {0.command_name} with request id "
-#                  "{0.request_id} started on server "
-#                  "{0.connection_id}".format(event))
-
-#     def succeeded(self, event):
-#         log.debug("Command {0.command_name} with request id "
-#                  "{0.request_id} on server {0.connection_id} "
-#                  "succeeded in {0.duration_micros} "
-#                  "microseconds".format(event))
-
-#     def failed(self, event):
-#         log.debug("Command {0.command_name} with request id "
-#                  "{0.request_id} on server {0.connection_id} "
-#                  "failed in {0.duration_micros} "
-#                  "microseconds".format(event))
-
-# monitoring.register(CommandLogger())
 
 class Db:
     Results = None
@@ -38,7 +14,6 @@ class Db:
         self.Rules = Rules
         self.Policies = Policies
         self.initConnection(config)
-        log.debug("Db(%s)", _connections)
 
     def initConnection(self, config):
         connect(
@@ -48,6 +23,6 @@ class Db:
             username=config.data['database']['username'],
             password=config.data['database']['password'],
             authentication_source=config.data['database']['dbName'],
-            maxPoolSize=5,
+            maxPoolSize=10,
             minPoolSize=1,
             connect=True)
