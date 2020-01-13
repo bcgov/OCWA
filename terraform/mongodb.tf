6731c1308df55227b9d7740432f0a0d8ede05304
BrandonSharratt <brandon@highwaythreesolutions.com>
2020-01-13 15:33:02 -0800

    drop tables before recreating

--- terraform/mongodb.tf
+++ terraform/mongodb.tf
@@ -89,7 +89,7 @@ resource "null_resource" "mongodb_formio_first_Time_install" {
     environment = {
       SCRIPT_PATH = var.hostRootPath
     }
-    command = "docker run --net=ocwa_vnet -v \"$SCRIPT_PATH/..:/work\" mongo:4.2.1 mongo mongodb://${var.mongodb["username"]}:${random_string.mongoSuperPassword.result}@ocwa_mongodb:27017/formioapp --eval db.submissions.drop(); db.forms.drop(); db.roles.drop();"
+    command = "docker run --net=ocwa_vnet -v \"$SCRIPT_PATH/..:/work\" mongo:4.2.1 mongo mongodb://${var.mongodb["username"]}:${random_string.mongoSuperPassword.result}@ocwa_mongodb:27017/formioapp --eval \"db.submissions.drop(); db.forms.drop(); db.roles.drop();\""
   }
 
 
