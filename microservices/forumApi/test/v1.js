aa5cbb14f7a55dbd9287aee0467879f04a824523
BrandonSharratt <brandon@highwaythreesolutions.com>
2020-05-05 11:44:58 -0700

    moved forum api

--- microservices/forumApi/test/v1.js
+++
@@ -1,8 +0,0 @@
-process.env.NODE_ENV = 'test';
-
-describe("V1", function() {
-    require('./v1/subscribers');
-    require('./v1/permissions');
-    require('./v1/topic');
-    require('./v1/comment');
-});
