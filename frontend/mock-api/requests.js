1778a7a2d00b9e6bf5a0d2bf56ab14c96c65f9e0
Joshua Jones <joshua@general-metrics.com>
2020-10-06 14:32:44 -0700

    Add mock server to help with dev testing.

---
+++ frontend/mock-api/requests.js
@@ -0,0 +1,69 @@
+module.exports = () => {
+  const requests = [];
+  // Create 1000 users
+  for (let i = 0; i < 300; i++) {
+    requests.push({
+      _id: i,
+      state: 2,
+      tags: [],
+      supportingFiles: [],
+      reviewers: ['josh-oc'],
+      files: ['63836f3343fc4817b47ec5c6c7d5227c'],
+      chronology: [
+        {
+          timestamp: '2019-04-15T17:55:45.355Z',
+          enteredState: 0,
+          change_by: 'pripley@idir',
+        },
+        {
+          timestamp: '2019-04-15T17:55:59.342Z',
+          enteredState: 1,
+          change_by: 'pripley@idir',
+          changes: { files: [] },
+        },
+        {
+          timestamp: '2019-04-15T17:56:11.367Z',
+          enteredState: 2,
+          change_by: 'pripley@idir',
+        },
+        {
+          timestamp: '2019-07-10T16:46:27.792Z',
+          enteredState: 3,
+          change_by: 'hodor',
+        },
+        {
+          timestamp: '2019-07-10T16:46:31.511Z',
+          enteredState: 1,
+          change_by: 'hodor',
+        },
+      ],
+      name: 'test22',
+      purpose: '',
+      variableDescriptions: '',
+      selectionCriteria: '',
+      steps: '',
+      freq: '',
+      confidentiality: '',
+      author: 'pripley@idir',
+      topic: '5cb4c5a1a5762c0035f02a31',
+      type: 'export',
+      formName: 'dataexport',
+      submittedDate: '2019-04-15T17:56:11.367Z',
+      projects: ['project_1'],
+      data: {
+        _id: '5cb4c5a1559ad10013ead3ae',
+        tags: [],
+        purpose: '',
+        variableDescriptions: '',
+        selectionCriteria: '',
+        steps: '',
+        freq: '',
+        confidentiality: '',
+        submittedDate: '2019-04-15T17:56:11.367Z',
+        projects: ['project_1'],
+      },
+    });
+  }
+
+  return { requests };
+};
