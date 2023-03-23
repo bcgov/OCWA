fcaffe1eeae781aad33e0de7068c6e756fb4ad07
BrandonSharratt <brandon@highwaythreesolutions.com>
2023-03-23 11:31:35 -0700

    tus patch

--- terraform/scripts/pre-create.py
+++ terraform/scripts/pre-create.py
@@ -11,6 +11,9 @@ for line in fileinput.input():
 
 js = json.loads(jsonStr)
 
+if 'Upload' in js:
+    js = js['Upload']
+
 if 'MetaData' in js:
     meta = js['MetaData']
     if 'jwt' in meta:
@@ -18,7 +21,7 @@ if 'MetaData' in js:
         jwtSecret = os.environ['JWT_SECRET']
         aud = os.environ['JWT_AUD']
         try:
-            jwt.decode(tok, jwtSecret, audience=aud)
+            jwt.decode(tok, jwtSecret, audience=aud, algorithms=["HS256"], options={'verify_exp': False})
             #Valid JWT
             exit(0)
         except Exception as e:
